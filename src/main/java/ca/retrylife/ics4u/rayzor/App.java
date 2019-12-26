b00d54b8eb66cfb14f19dec1422c2a489408ff54
Evan Pratten <ewpratten@gmail.com>
2019-12-26 13:58:05 -0500

    Added java project

---
+++ src/main/java/ca/retrylife/ics4u/rayzor/App.java
@@ -0,0 +1,24 @@
+package ca.retrylife.ics4u.rayzor;
+
+public class App implements Runnable{
+
+    public static void main(String[] args) {
+        
+        // Build an app
+        App app = new App();
+
+        // Run the app
+        app.run();
+    }
+
+    private App(){
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+
+}
